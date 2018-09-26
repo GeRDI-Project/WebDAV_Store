5272ef072eaaeff196b8ef2115995dde08f2c154
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2018-09-26 18:06:23 +0200

    SAI-1023 adopted api changes

--- src/main/java/de/gerdiproject/store/impl/WebDAVStoreService.java
+++ src/main/java/de/gerdiproject/store/impl/WebDAVStoreService.java
@@ -19,16 +19,15 @@ import com.github.sardine.DavResource;
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
 import de.gerdiproject.store.AbstractStoreService;
+import de.gerdiproject.store.datamodel.CopyStatus;
 import de.gerdiproject.store.datamodel.ListElement;
 import de.gerdiproject.store.datamodel.ResearchDataInputStream;
-import de.gerdiproject.store.datamodel.TaskElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -82,35 +81,30 @@ public class WebDAVStoreService extends AbstractStoreService<WebDAVCreds> {
 
 
     @Override
-    protected boolean copyFile(final WebDAVCreds creds, final String targetDir, final TaskElement taskElement) {
+    protected boolean copyFile(final WebDAVCreds creds, final String targetDir, final ResearchDataInputStream inputStream) {
         final URL url;
-        try {
-            url = new URL(taskElement.getFileName());
-        } catch (MalformedURLException e) {
-            LOGGER.error("Encountered malformed URL.", e);
-            return false;
-        }
-        final String[] splitFile = url.getFile().split("/");
+        final String[] splitFile = inputStream.getName().split("/");
         final String fileName = splitFile[splitFile.length - 1];
         final Sardine client = creds.getClient();
         final String appendedDir = creds.getDomain().concat(targetDir);
         try {
             if (client.exists(appendedDir + fileName)) { // TODO: new file name with with a suffix
-                taskElement.setProgressInPercent(-2); // File already exists
+                inputStream.setStatus(CopyStatus.ERROR); // File already exists
             } else {
                 new Thread(() -> {
                     try {
-                        client.put(appendedDir + fileName, new ResearchDataInputStream(url, taskElement));
+                        inputStream.setStatus(CopyStatus.RUNNING);
+                        client.put(appendedDir + fileName, inputStream);
                     } catch (IOException e) {
-                        taskElement.setProgressInPercent(-2);
+                        inputStream.setStatus(CopyStatus.ERROR);
                         return;
                     }
-                    taskElement.setProgressInPercent(100);
+                    inputStream.setStatus(CopyStatus.FINISHED);
                 }).start();
             }
         } catch (IOException e) {
             LOGGER.error("Error while copying files.", e);
-            taskElement.setProgressInPercent(-2); // Set to -2 as there is an error with this file
+            inputStream.setStatus(CopyStatus.ERROR); // Set to -2 as there is an error with this file
             return false;
         }
         return true;
